f9292e14772a6b1dc0d7ffbae1bd09919557b0e2
RajrupDasid <rajrupdasid@keemail.me>
2023-02-19 10:12:47 +0530

    main views

---
+++ resources/views/listing.blade.php
@@ -0,0 +1,11 @@
+<h1>Listing</h1>
+@unless (count($listings)==0)
+    @foreach($listings as $listing)
+    <h2>
+       <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
+    </h2>
+    <p>
+        {{$listing['description']}}
+    </p>
+    @endforeach
+@endunless
