111ac7b790d85b8f4a743bdfa0c508e3661191f2
RajrupDasid <rajrupdasid@keemail.me>
2023-02-19 11:39:38 +0530

    dummy

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,28 +1,11 @@
 <?php
-namespace App\Models;
- class Listing{
-    public static function all(){
-        return[
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Description One'
-            ],
-            [
-                'id' => 2 ,
-                'title' => 'Listing Two',
-                'description' => 'Description Two'
-            ],
-        ];
-    }
-    public static function find($id){
-        $listings = self::all();
 
-        foreach($listings as $listings){
-            if($listings['id']==$id){
-                return $listings;
-            }
-        }
-    }
- }
-?>
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+}
