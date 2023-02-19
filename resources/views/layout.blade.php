9fd574a5957ad695fa907a0cd95feda5c32f4df2
RajrupDasid <rajrupdasid@keemail.me>
2023-02-19 16:09:58 +0530

    main blade .php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -29,7 +29,7 @@
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
             <a href="index.html"
-                ><img class="w-24" src="images/logo.png" alt="" class="logo"
+                ><img class="w-24" src="{{asset('public/images/logo.png')}}" alt="" class="logo"
             /></a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 <li>
@@ -45,7 +45,9 @@
                 </li>
             </ul>
         </nav>
+        <main>
     @yield('content')
+        </main>
 
     <footer
             class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
