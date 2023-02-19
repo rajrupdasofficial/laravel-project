b79a9a00949cacbd989af8a639891ef89f570196
RajrupDasid <rajrupdasid@keemail.me>
2023-02-19 18:09:04 +0530

    added component related viewsets

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class Listing extends Model
 {
     use HasFactory;
+
+    public function scopeFilter($query, array $filters){
+        if($filters['tag']?? false){
+            $query->where('tags','like','%' . request('tag') . '%');
+        }
+    }
 }
