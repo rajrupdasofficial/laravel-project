62e3d8c85e55adb30b07a648c7534a9468ac435c
RajrupDasid <rajrupdasid@keemail.me>
2023-02-19 22:07:30 +0530

    logo column added to main table

--- database/migrations/2023_02_19_045058_create_listings_table.php
+++ database/migrations/2023_02_19_045058_create_listings_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('logo');
             $table->string('tags');
             $table->string('company');
             $table->string('location');
