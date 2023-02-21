bb2847ad5bb7e5860821aab356f367933b18f4eb
RajrupDasid <rajrupdasid@keemail.me>
2023-02-21 21:05:34 +0530

    edit added

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,13 @@ Route::get('/listings/create',[ListingController::class,'create']);
 Route::post('/listings',[ListingController::class, 'store']);
 
 
+
+//show listing 
+Route::get('listings/{listing}/edit',[ListingController::class, 'edit']);
+
+//Update Listing
+Route::put('listings/{listing}',[ListingController::class,'update']);
+
 //single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show' ]);
 
