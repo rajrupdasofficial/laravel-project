558dc850ee8173f20f6eb5d1200fe5d87e321bfd
RajrupDasid <rajrupdasid@keemail.me>
2023-02-21 21:04:33 +0530

    Edit / PUT method added

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -51,4 +51,32 @@ class ListingController extends Controller
 
         return redirect('/')->with('message','Listing created successfully');
     }
+    //show edut form
+    public function edit(Listing $listing){
+        return view('listings.edit', ['listing'=> $listing]);
+    }
+
+
+    //show update data
+    public function update(Request $request, Listing $listing){
+        $formFields = $request->validate([
+            'title'=> 'required',
+           // 'logo' => 'required|image|mimes:png,jpg,jpeg',
+            'company'=>['required'],
+            'location'=>'required',
+            'website'=>'required',
+            'email'=>['required','email'],
+            'tags'=>'required',
+            'description'=>'required',   
+        ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+        //Session::flash('message',);
+
+        return back()->with('message','Listing updated successfully');
+    }
 }
