f10b16c72ad44d667ed7f30c2f99ddf57811ec56
RajrupDasid <rajrupdasid@keemail.me>
2023-02-23 21:39:49 +0530

    added register route and added UserController

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListingController;
-use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -50,4 +51,7 @@ Route::delete('/listings/{listing}',[ListingController::class, 'destroy']);
 
 //single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show' ]);
+
+//show register create form
+Route::get('/register/',[UserController::class,'register']);
 
